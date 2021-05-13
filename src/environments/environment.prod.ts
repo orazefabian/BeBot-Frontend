a6da220e0b9d26ebb6c128dfc8191ccf74ce294e
Fabian Oraze <oraze.fabian@icloud.com>
2021-05-13 22:50:26 +0200

    added api url to prod  environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+};
+
+// bebot server inside the docker container published on port 6969
+export const BeBotServer = {
+  baseURL: '192.168.8.99:6969',
 };
