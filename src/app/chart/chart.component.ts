625c2ee2abf45e8f3230b8a1040ed50ad614ed0c
Fabian Oraze <oraze.fabian@icloud.com>
2021-05-16 21:21:16 +0200

    some additions to chart module

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ResourceService } from '../resource.service';
 import Chart from 'chart.js/auto';
+import { PreviousStats } from '../../data/previousStats';
 
 @Component({
   selector: 'app-chart',
@@ -10,64 +11,74 @@ import Chart from 'chart.js/auto';
 export class ChartComponent implements OnInit {
   constructor(private service: ResourceService) {}
 
+  prevData: PreviousStats;
   myChart: Chart;
-  backgroundColors = [
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(255, 206, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-  ];
-  borderColors = [
-    'rgba(54, 162, 235, 1)',
-    'rgba(255, 99, 132, 1)',
-    'rgba(255, 206, 86, 1)',
-    'rgba(75, 192, 192, 1)',
-    'rgba(153, 102, 255, 1)',
-    'rgba(255, 159, 64, 1)',
-  ];
 
   ngOnInit(): void {
-    this.myChart = new Chart('prevStats', {
-      type: 'bar',
-      data: {
-        labels: ['last 7 days', 'last 14 days', 'last 30 days'],
-        datasets: [
-          {
-            label: 'won predictions',
-            data: [12, 34, 49],
-            backgroundColor: 'rgba(233,0,0,0.8)',
+    this.service.getPreviousStats().subscribe((result) => {
+      this.prevData = result;
+
+      this.myChart = new Chart('prevStats', {
+        type: 'bar',
+        data: {
+          labels: ['last 7 days', 'last 14 days', 'last 30 days'],
+          datasets: [
+            {
+              label: 'lost predictions',
+              data: [
+                this.prevData.data.details.last_7_days.lost,
+                this.prevData.data.details.last_14_days.lost,
+                this.prevData.data.details.last_30_days.lost,
+              ],
+              backgroundColor: 'rgba(255,0,0,0.5)',
+              borderColor: 'rgba(255,0,0)',
+              borderWidth: 3,
+              borderSkipped: false,
+            },
+            {
+              label: 'won predictions',
+              data: [
+                this.prevData.data.details.last_7_days.won,
+                this.prevData.data.details.last_14_days.won,
+                this.prevData.data.details.last_30_days.won,
+              ],
+              backgroundColor: 'rgba(0,255,0,0.5)',
+              borderColor: 'rgba(0,255,0)',
+              borderWidth: 3,
+              borderSkipped: false,
+            },
+            {
+              label: 'pending predictions',
+              data: [
+                this.prevData.data.details.last_7_days.pending,
+                this.prevData.data.details.last_14_days.pending,
+                this.prevData.data.details.last_30_days.pending,
+              ],
+              backgroundColor: 'rgba(0,0,255,0.5)',
+              borderColor: 'rgba(0,0,255)',
+              borderWidth: 3,
+              borderSkipped: false,
+            },
+          ],
+        },
+        options: {
+          plugins: {
+            title: {
+              display: true,
+              text: 'Chart.js Bar Chart - Stacked',
+            },
           },
-          {
-            label: 'lost predictions',
-            data: [8, 30, 34],
-            backgroundColor: 'rgba(0,255,0,1)',
-          },
-          {
-            label: 'pending predictions',
-            data: [2, 3, 6],
-            backgroundColor: 'rgba(0,0,255,1)',
-          },
-        ],
-      },
-      options: {
-        plugins: {
-          title: {
-            display: true,
-            text: 'Chart.js Bar Chart - Stacked',
+          responsive: true,
+          scales: {
+            x: {
+              stacked: true,
+            },
+            y: {
+              stacked: true,
+            },
           },
         },
-        responsive: true,
-        scales: {
-          x: {
-            stacked: true,
-          },
-          y: {
-            stacked: true,
-          },
-        },
-      },
+      });
     });
   }
 }
